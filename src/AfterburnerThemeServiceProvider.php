4b6f8b5a00f9a3b60b676e5a92c49efd815d9eee
Ryan Thompson <service@aiwebsystems.com>
2019-09-09 01:36:33 -0500

    Testing things out..

--- src/AfterburnerThemeServiceProvider.php
+++ src/AfterburnerThemeServiceProvider.php
@@ -1,7 +1,8 @@
 <?php namespace Pyrocms\AfterburnerTheme;
 
 use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
-use Illuminate\Routing\Router;
+use Anomaly\Streams\Platform\Ui\ControlPanel\Component\Navigation\Event\SortNavigation;
+use Pyrocms\AfterburnerTheme\Listener\ApplySorting;
 
 /**
  * Class AfterburnerThemeServiceProvider
@@ -14,157 +15,23 @@ class AfterburnerThemeServiceProvider extends AddonServiceProvider
 {
 
     /**
-     * Additional addon plugins.
-     *
-     * @type array|null
-     */
-    protected $plugins = [];
-
-    /**
-     * The addon Artisan commands.
-     *
-     * @type array|null
-     */
-    protected $commands = [];
-
-    /**
-     * The addon's scheduled commands.
-     *
-     * @type array|null
-     */
-    protected $schedules = [];
-
-    /**
-     * The addon API routes.
-     *
-     * @type array|null
-     */
-    protected $api = [];
-
-    /**
-     * The addon routes.
-     *
-     * @type array|null
-     */
-    protected $routes = [];
-
-    /**
-     * The addon middleware.
-     *
-     * @type array|null
-     */
-    protected $middleware = [
-        //Pyrocms\AfterburnerTheme\Http\Middleware\ExampleMiddleware::class
-    ];
-
-    /**
-     * Addon group middleware.
+     * The addon listeners.
      *
      * @var array
      */
-    protected $groupMiddleware = [
-        //'web' => [
-        //    Pyrocms\AfterburnerTheme\Http\Middleware\ExampleMiddleware::class,
-        //],
-    ];
-
-    /**
-     * Addon route middleware.
-     *
-     * @type array|null
-     */
-    protected $routeMiddleware = [];
-
-    /**
-     * The addon event listeners.
-     *
-     * @type array|null
-     */
     protected $listeners = [
-        //Pyrocms\AfterburnerTheme\Event\ExampleEvent::class => [
-        //    Pyrocms\AfterburnerTheme\Listener\ExampleListener::class,
-        //],
+        SortNavigation::class => [
+            ApplySorting::class,
+        ],
     ];
 
     /**
-     * The addon alias bindings.
+     * The view overrides.
      *
-     * @type array|null
-     */
-    protected $aliases = [
-        //'Example' => Pyrocms\AfterburnerTheme\Example::class
-    ];
-
-    /**
-     * The addon class bindings.
-     *
-     * @type array|null
-     */
-    protected $bindings = [];
-
-    /**
-     * The addon singleton bindings.
-     *
-     * @type array|null
-     */
-    protected $singletons = [];
-
-    /**
-     * Additional service providers.
-     *
-     * @type array|null
-     */
-    protected $providers = [
-        //\ExamplePackage\Provider\ExampleProvider::class
-    ];
-
-    /**
-     * The addon view overrides.
-     *
-     * @type array|null
+     * @var array
      */
     protected $overrides = [
-        //'streams::errors/404' => 'module::errors/404',
-        //'streams::errors/500' => 'module::errors/500',
+        'streams::table.table' => 'pyrocms.theme.afterburner::streams.table.table',
     ];
-
-    /**
-     * The addon mobile-only view overrides.
-     *
-     * @type array|null
-     */
-    protected $mobile = [
-        //'streams::errors/404' => 'module::mobile/errors/404',
-        //'streams::errors/500' => 'module::mobile/errors/500',
-    ];
-
-    /**
-     * Register the addon.
-     */
-    public function register()
-    {
-        // Run extra pre-boot registration logic here.
-        // Use method injection or commands to bring in services.
-    }
-
-    /**
-     * Boot the addon.
-     */
-    public function boot()
-    {
-        // Run extra post-boot registration logic here.
-        // Use method injection or commands to bring in services.
-    }
-
-    /**
-     * Map additional addon routes.
-     *
-     * @param Router $router
-     */
-    public function map(Router $router)
-    {
-        // Register dynamic routes here for example.
-        // Use method injection or commands to bring in services.
-    }
 
 }
