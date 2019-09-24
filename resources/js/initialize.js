44ab50ebacb9ca1c760c3e5deb99f9c8ff2c83ed
Ryan Thompson <ryan@pyrocms.com>
2019-09-24 02:55:23 -0500

    Meh'

--- resources/js/initialize.js
+++ resources/js/initialize.js
@@ -11,9 +11,9 @@ Vue.config.productionTip = false;
 new Vue({
     render: h => h(App),
     mounted() {
-        // You are able to access plugin from everywhere via this.$toastr
-        this.$toastr.defaultPosition = "toast-top-left";
-        // Send message to browser screen
+
+        this.$toastr.defaultPosition = "toast-bottom-right";
+
         this.$toastr.s(
             "This Message From Toastr Plugin\n You can access this plugin : <font color='yellow'>this.$toastr</font>"
         );
