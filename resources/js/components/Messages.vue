a64b4c3f51ff31ac6cf29aac3737369fd63a7a16
Ryan Thompson <service@aiwebsystems.com>
2019-09-24 08:58:41 -0500

    Stuff

---
+++ resources/js/components/Messages.vue
@@ -0,0 +1,45 @@
+<template>
+    <vue-toastr ref="mytoast"></vue-toastr>
+</template>
+
+<script>
+
+    import VueToastr from "vue-toastr";
+
+    export default {
+        name: "toaster",
+        components: {VueToastr},
+        props: {
+            open: {
+                type: Boolean,
+                required: true
+            },
+            message: {
+                type: String,
+                required: false
+            }
+        },
+        data() {
+            return {
+                variable: ""
+            };
+        },
+        created() {},
+        mounted() {
+            this.$refs.mytoast.defaultPosition = "toast-top-left";
+            this.$refs.mytoast.s(
+                this.message
+            );
+            console.log(this.$refs.mytoast);
+        },
+        watch: {
+            data(newVal, oldVal) {}
+        },
+        computed: {
+            aname() {}
+        },
+        methods: {
+            methodname() {}
+        }
+    };
+</script>
--- resources/js/initialize.js
+++ resources/js/initialize.js
@@ -1,22 +1,3 @@
-import Vue from "vue";
-import VueToastr from "vue-toastr";
+import Messages from './components/Messages.vue';
 
-// use plugin
-Vue.use(VueToastr, {
-    /* OverWrite Plugin Options if you need */
-});
-
-Vue.config.productionTip = false;
-
-new Vue({
-    render: h => h(App),
-    mounted() {
-
-        this.$toastr.defaultPosition = "toast-bottom-right";
-
-        this.$toastr.s(
-            "This Message From Toastr Plugin\n You can access this plugin : <font color='yellow'>this.$toastr</font>"
-        );
-    }
-}).$mount("vue-toastr");
-
+Vue.component('messages', Messages);
