83ea6a827db80f6a1e4c7d361bef292628e5affe
Ryan Thompson <ryan@pyrocms.com>
2019-10-09 00:49:51 -0500

    Blade

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,28 +1,27 @@
-{{-- <section id="messages" class="fixed bottom-0 right-0">
+<section id="messages" class="fixed bottom-0 right-0">
 
-    {% if message_exists('success') %}
-        {% for message in message_pull('success') %}
-            <toaster :open="true" type="success" {#class="p-6 w-72 bg-green-500 text-green-100 color-white rounded mr-3 mb-3"#} message="{{ trans(message)|markdown }}"></toaster>
-        {% endfor %}
-    {% endif %}
+    @if (app('messages')->has('success'))
+        @foreach (app('messages')->pull('success') as $message)
+            <toaster :open="true" type="success" class="p-6 w-72 bg-green-500 text-green-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>  
+        @endforeach
+    @endif
 
-    {% if message_exists('info') %}
-        {% for message in message_pull('info') %}
-            <toaster :open="true" timeout="0" type="info" {#class="p-6 w-72 bg-info-500 text-info-100 color-white rounded mr-3 mb-3"#} message="{{ trans(message)|markdown }}"></toaster>
-        {% endfor %}
-    {% endif %}
+    @if (app('messages')->has('info'))
+        @foreach (app('messages')->pull('info') as $message)
+            <toaster :open="true" type="info" class="p-6 w-72 bg-blue-500 text-green-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>  
+        @endforeach
+    @endif
 
-    {% if message_exists('warning') %}
-        {% for message in message_pull('warning') %}
-            <toaster :open="true" timeout="0" type="warning" {#class="p-6 w-72 bg-orange-500 text-orange-100 color-white rounded mr-3 mb-3"#} message="{{ trans(message)|markdown }}"></toaster>
-        {% endfor %}
-    {% endif %}
+    @if (app('messages')->has('warning'))
+        @foreach (app('messages')->pull('warning') as $message)
+            <toaster :open="true" type="warning" class="p-6 w-72 bg-blue-500 text-yellow-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>  
+        @endforeach
+    @endif
 
-    {% if message_exists('error') %}
-        {% for message in message_pull('error') %}
-            <toaster :open="true" timeout="0" type="error" {#class="p-6 w-72 bg-orange-500 text-orange-100 color-white rounded mr-3 mb-3"#} message="{{ trans(message)|markdown }}"></toaster>
-        {% endfor %}
-    {% endif %}
+    @if (app('messages')->has('error'))
+        @foreach (app('messages')->pull('error') as $message)
+            <toaster :open="true" type="error" class="p-6 w-72 bg-orange-500 text-yellow-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>  
+        @endforeach
+    @endif
 
 </section>
---}}
